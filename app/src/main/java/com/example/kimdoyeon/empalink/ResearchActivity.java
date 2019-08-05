f2da8cbd2253566eac6ee9bd910661f0eb0472c3
CSEKimDoYeon <ehdus0008@naver.com>
2019-08-06 01:39:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kimdoyeon/empalink/ResearchActivity.java
@@ -0,0 +1,17 @@
+package com.example.kimdoyeon.empalink;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.empatica.sample.R;
+
+public class ResearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_research);
+
+    }
+}
